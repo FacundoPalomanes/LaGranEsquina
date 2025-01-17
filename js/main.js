859cf9aec7afa355b277073e04043af6fee8c197
facundop <facpalomanes@gmail.com>
2025-01-17 16:37:16 -0300

    fix: :sparkles: carousel image rezising is better and try to do animation in phones menus

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.querySelector('.navbar__toggle-btn');
     const mobileMenu = document.querySelector('.navbar__mobile-menu');
+    const movePage = document.querySelector('#move-page');
 
     const toggleMenu = () => {
         mobileMenu.style.display = mobileMenu.style.display === "none" || mobileMenu.style.display === "" ? "flex" : "none";
-    }
+        mobileMenu.classList.toggle('open');
+    };
 
     const hideMenuResize = () => {
         mobileMenu.style.display = "none";
-        carousel.classList.remove('shifted');
+        movePage.classList.remove('shifted');
     }
 
     toggleButton.addEventListener("click", toggleMenu);
     window.addEventListener("resize", hideMenuResize);
     window.addEventListener("load", hideMenuResize);
 
-    const carousel = document.querySelector('#carouselExampleInterval');
   
     toggleButton.addEventListener('click', () => {
       mobileMenu.classList.toggle('open'); // Alterna el estado visible del menú
-      carousel.classList.toggle('shifted'); // Mueve el carrusel hacia abajo
+      movePage.classList.toggle('shifted'); // Mueve el carrusel hacia abajo
     });
 });
