755952c0aa7d62bb9a59f390899a9ee5247c4d00
facundop <facpalomanes@gmail.com>
2025-01-24 14:04:40 -0300

    feat: :sparkles: dark mode feature

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.querySelector(".navbar__toggle-btn");
+  const toggleButton = document.querySelector("#toggle-menu");
   const mobileMenu = document.querySelector(".navbar__mobile-menu");
   const movePage = document.querySelector("#move-page");
   const prev = document.querySelector("#prev");
@@ -10,20 +10,26 @@ document.addEventListener("DOMContentLoaded", () => {
     movePage.classList.remove("shifted");
   };
 
+  // Toggle dark mode 
+  document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  }); 
+
+  document.getElementById('dark-mode-toggle-mobile').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  }); 
+
   // toggleButton.addEventListener("click", toggleMenu);
   window.addEventListener("resize", hideMenuResize);
   window.addEventListener("load", hideMenuResize);
 
   toggleButton.addEventListener("click", () => {
-    mobileMenu.style.display =
-      mobileMenu.style.display === "none" || mobileMenu.style.display === ""
-        ? "flex"
-        : "none";
+    mobileMenu.style.display = mobileMenu.style.display === "none" || mobileMenu.style.display === "" ? "flex" : "none";
     mobileMenu.classList.toggle("open"); // Alterna el estado visible del menú
     movePage.classList.toggle("shifted");
   });
 
-  // Add click event to cards
+  // Add click event to cards product
   prev.addEventListener("click", () => {
     console.log("prev");
     const container = document.querySelector(".scroll-container");
