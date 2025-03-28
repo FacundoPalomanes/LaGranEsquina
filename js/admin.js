fc41b72e09f741647f33c3e701474124843e3598
FacundoPalomanes <facpalomanes@gmail.com>
2025-03-28 19:57:23 -0300

    fix: :bug: admin bug, only render things when the page had the components

--- js/admin.js
+++ js/admin.js
@@ -95,55 +95,58 @@ document.addEventListener("DOMContentLoaded", async () => {
       defaultLink.classList.add("active");
       document.getElementById("excel").classList.add("active");
       document.getElementById("excel").style.display = "block";
+      enablePageThings();
     }
   
-    const messageDiv = document.getElementById("message");
+    function enablePageThings() {
+        const messageDiv = document.getElementById("message");
   
-    document
-      .getElementById("uploadForm")
-      .addEventListener("submit", async function (e) {
-        e.preventDefault();
-  
-        const formData = new FormData();
-        const fileInput = document.getElementById("fileInput");
-        formData.append("file", fileInput.files[0]);
-  
-        const response = await fetch(`${fetchUrl}/upload`, {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${jwt}`,
-          },
-          body: formData,
-        });
-  
-        if (response.ok) {
-          messageDiv.classList.remove("error");
-          messageDiv.classList.add("success");
-          messageDiv.textContent = "Archivo subido exitosamente.";
-        } else {
-          messageDiv.classList.remove("success");
-          messageDiv.classList.add("error");
-          messageDiv.textContent = "Hubo un error al subir el archivo.";
-        }
-        messageDiv.style.display = "block";
-      });
-      document.getElementById("close-session").addEventListener("click", () => {
-          // Borrar el JWT del localStorage
-          localStorage.removeItem("jwt");
+        document
+          .getElementById("uploadForm")
+          .addEventListener("submit", async function (e) {
+            e.preventDefault();
       
-          // Mostrar el formulario de inicio de sesión
-          document.getElementById("form").classList.remove("display-none");
-          
-          // Limpiar el contenido del contenedor admin
-          const container = document.getElementById("adminContainer");
-          container.innerHTML = "";
-          
-          // Limpiar el mensaje
-          document.getElementById("message").textContent = "";
+            const formData = new FormData();
+            const fileInput = document.getElementById("fileInput");
+            formData.append("file", fileInput.files[0]);
       
-          // Redirigir a la página principal o login
-          window.location.href = '/index.html'; // Redirigir a la página de inicio de sesión
-      });
+            const response = await fetch(`${fetchUrl}/upload`, {
+              method: "POST",
+              headers: {
+                Authorization: `Bearer ${jwt}`,
+              },
+              body: formData,
+            });
+      
+            if (response.ok) {
+              messageDiv.classList.remove("error");
+              messageDiv.classList.add("success");
+              messageDiv.textContent = "Archivo subido exitosamente.";
+            } else {
+              messageDiv.classList.remove("success");
+              messageDiv.classList.add("error");
+              messageDiv.textContent = "Hubo un error al subir el archivo.";
+            }
+            messageDiv.style.display = "block";
+          });
+          document.getElementById("close-session").addEventListener("click", () => {
+              // Borrar el JWT del localStorage
+              localStorage.removeItem("jwt");
+          
+              // Mostrar el formulario de inicio de sesión
+              document.getElementById("form").classList.remove("display-none");
+              
+              // Limpiar el contenido del contenedor admin
+              const container = document.getElementById("adminContainer");
+              container.innerHTML = "";
+              
+              // Limpiar el mensaje
+              document.getElementById("message").textContent = "";
+          
+              // Redirigir a la página principal o login
+              window.location.href = '/index.html'; // Redirigir a la página de inicio de sesión
+          });
+    }
   
     });
     // Cerrar sesión
